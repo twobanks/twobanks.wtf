47aa4026af5a86e67c2a1df2301baa623ec248f3
twobanks <twobanks@me.com>
2022-10-21 17:38:46 -0300

    fix: avatar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { pages } from './mock'
 import { conversionPage } from '../../utils/functions/conversionPage'
-const twobanks = '/img/twobanks.png';
+const avatar = '/img/avatar.webp';
 
 const Header = () => {
   const [hovered, setHovered] = useState<string>('')
@@ -13,7 +13,7 @@ const Header = () => {
       <S.Content>
         <S.Banks>
           <Link href="/" passHref>
-            <Image src={twobanks} alt="twobanks" placeholder="blur" blurDataURL={twobanks} height={60} width={60} />
+            <Image src={avatar} alt="twobanks" placeholder="blur" blurDataURL={avatar} height={60} width={60} />
           </Link>
         </S.Banks>
         <S.Nav>
