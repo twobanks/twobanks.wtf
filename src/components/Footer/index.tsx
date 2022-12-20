02fc2dc4392159109e54ccc147adc4e3c5eb20f4
twobanks <twobanks@me.com>
2022-12-20 18:48:41 -0300

    add: icon animation footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,24 @@
 
 import Icon from '../Icon';
-import * as S from './styled'
+import * as S from './styled';
+import { useState } from 'react';
 import Link from 'next/link';
 import { menuItems } from './mock'
 
 const Footer = () => {
+  const [hovered, setHovered] = useState<string>('');
   return (
     <S.Footer>
-      {menuItems.map(item => (
-        <Link href={item.url} passHref key={item.id}>
-          <li>
-            <Icon src={item.icon} alt={item.name} />
-          </li>
-        </Link>
-      ))}
+      {menuItems.map(item => {
+        const isHovered = hovered === item.name;
+        return (
+          <Link href={item.url} passHref key={item.id}>
+            <li onMouseEnter={() => setHovered(item.name)} onMouseLeave={() => setHovered('')}>
+              <Icon src={isHovered ? item.iconAnimated : item.icon} alt={item.name} />
+            </li>
+          </Link>
+        )
+      })}
     </S.Footer>
   )
 }
--- src/components/Footer/mock.ts
+++ src/components/Footer/mock.ts
@@ -1,16 +1,22 @@
 import { v4 as uuid} from 'uuid';
 
-const home = '/icon/home.svg';
-const article = '/icon/article.svg';
-const activities = '/icon/activities.svg';
-const book = '/icon/book.svg';
-const code = '/icon/code.svg';
+const home = '/icon/animation/home.svg';
+const article = '/icon/animation/article.svg';
+const activities = '/icon/animation/activities.svg';
+const book = '/icon/animation/book.svg';
+const code = '/icon/animation/code.svg';
+const homeAnimated = '/icon/animation/home-animated.webp';
+const articleAnimated = '/icon/animation/article-animated.webp';
+const activitiesAnimated = '/icon/animation/activities-animated.webp';
+const bookAnimated = '/icon/animation/book-animated.webp';
+const codeAnimated = '/icon/animation/code-animated.webp';
 
 type ItemsProps = {
   id: string;
   name: string;
   url: string;
   icon: string;
+  iconAnimated: string;
 }
 
 export const menuItems: ItemsProps[] = [
@@ -19,29 +25,34 @@ export const menuItems: ItemsProps[] = [
     name: 'home',
     url: '/',
     icon: home,
+    iconAnimated: homeAnimated,
   },
   {
     id: uuid(),
     name: 'sobre',
     url: '/about',
     icon: book,
+    iconAnimated: bookAnimated,
   },
   {
     id: uuid(),
     name: 'atividades',
     url: '/activities',
     icon: activities,
+    iconAnimated: activitiesAnimated,
   },
   {
     id: uuid(),
     name: 'trampos',
     url: '/works',
     icon: code,
+    iconAnimated: codeAnimated,
   },
   {
     id: uuid(),
     name: 'blog',
     url: '/idea',
     icon: article,
+    iconAnimated: articleAnimated,
   },
 ]
