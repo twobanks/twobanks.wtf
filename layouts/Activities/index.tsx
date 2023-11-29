24c1b295caed4cae001ee73f6e657d4f7c148e34
twobanks <twobanks@me.com>
2023-11-29 02:15:53 -0300

    add: filter activities

--- layouts/Activities/index.tsx
+++ layouts/Activities/index.tsx
@@ -5,6 +5,7 @@ import * as S from './styled'
 import { useState } from 'react';
 import images from '@/public';
 import Image from 'next/image';
+import { ACTIVITY } from '@/utils/enums/strava';
 
 type ActivitiesProps = {
   activities: Activities[];
@@ -12,7 +13,8 @@ type ActivitiesProps = {
 
 const ActivitiesTemplate = ({ activities }: ActivitiesProps) => {
   const [options, setOptions] = useState<'activities' | 'competitions'>('activities');
-  const [type, setType] = useState<'all' | 'bike' | 'trail' | 'gym'>('all');
+  const [type, setType] = useState<ACTIVITY>(ACTIVITY.RUN);
+  const activitiesFiltered = activities.filter(item => item.type === type);
   return (
     <S.Content>
       <S.Options>
@@ -23,18 +25,18 @@ const ActivitiesTemplate = ({ activities }: ActivitiesProps) => {
           Competições
         </button>
         |
-        <button type='button' onClick={() => setType('trail')}>
-          <Image src={images.trail} alt="trail" height={20} width={20} blurDataURL={images.trail} priority quality={100} />
+        <button type='button' onClick={() => setType(ACTIVITY.RUN)}>
+          <Image src={images.trail} alt={ACTIVITY.RUN} height={20} width={20} blurDataURL={images.trail} priority quality={100} />
         </button>
-        <button type='button' onClick={() => setType('bike')}>
-          <Image src={images.bike} alt="bike" height={20} width={20} blurDataURL={images.bike} priority quality={100} />
+        <button type='button' onClick={() => setType(ACTIVITY.RIDE)}>
+          <Image src={images.bike} alt={ACTIVITY.RIDE} height={20} width={20} blurDataURL={images.bike} priority quality={100} />
         </button>
-        <button type='button' onClick={() => setType('gym')} disabled={options === 'activities'}>
-          <Image src={images.workout} alt="workout" height={20} width={20} blurDataURL={images.workout} priority quality={100} />
+        <button type='button' onClick={() => setType(ACTIVITY.GYM)}>
+          <Image src={images.workout} alt={ACTIVITY.GYM} height={20} width={20} blurDataURL={images.workout} priority quality={100} />
         </button>
       </S.Options>
       <S.WrapperChallenges>
-        {options === 'activities' && <Strava activities={activities} />}
+        {options === 'activities' && <Strava activities={activitiesFiltered} />}
         {options === 'competitions' && <Challenges challenges={challenges} completedChallenges={completedChallenges} />}
       </S.WrapperChallenges>
     </S.Content>
