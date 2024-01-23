bb9f2746bb36f695054ce12aff02d493085edb8d
twobanks <twobanks@me.com>
2024-01-23 13:32:22 -0300

    add: analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "@/styles/global";
 import theme from '@/styles/theme';
+import { Analytics } from '@vercel/analytics/react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Component {...pageProps}  />
+    <Analytics debug={false} />
   </ThemeProvider>
 )
 
