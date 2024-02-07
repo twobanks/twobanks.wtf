157bf41b686a5158ddcd92383d7ea106cff59b46
twobanks <twobanks@me.com>
2024-02-07 17:03:52 -0300

    add: shooting stars

--- layouts/Wrapper/index.tsx
+++ layouts/Wrapper/index.tsx
@@ -11,6 +11,7 @@ type WrapperProps = {
 const Wrapper = ({ page, children }: WrapperProps) => (
   <S.Wrapper>
     <S.Space />
+    {[0, 1, 2, 3, 4, 5, 6, 7].map(index => <S.Star key={`star-${index}`} />)}
     <Header page={page} />
     <S.Content $page={page}>
       {children}
--- layouts/Wrapper/styled.ts
+++ layouts/Wrapper/styled.ts
@@ -11,6 +11,7 @@ export const Wrapper = styled.main`
   flex-direction: column;
   width: 100%;
   min-height: 100vh;
+  overflow: hidden;
 `;
 
 export const Space = styled.div`
@@ -51,4 +52,98 @@ export const Content = styled.section<WrapperProps>`
       padding: 0 ${theme.spacing.s2};
     `}
   `}
+`;
+
+export const Star = styled.span`
+  @keyframes animate {
+    0%
+    {
+        transform: rotate(315deg) translateX(0);
+        opacity: 1;
+    }
+    70%
+    {
+        opacity: 1;
+
+    }
+    100%
+    {
+        transform: rotate(315deg) translateX(-1500px);
+        opacity: 0;
+
+    }
+    
+  }
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 4px;
+  height: 4px;
+  background: #fff;
+  border-radius: 50%;
+  box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.1), 0 0 0 8px rgba(255, 255, 255, 0.1), 0 0 20px rgba(255, 255, 255, 1)  ;
+  animation:  animate 3s linear infinite;
+  &::before{
+    content: '';
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 300px;
+    height: 1px;
+    background: linear-gradient(90deg, #fff, transparent);
+  }
+  &:nth-child(1){
+    top: 0;
+    right: 0;
+    left:initial;
+    animation-delay:0 ;
+    animation-duration: 1s;
+  }
+
+  &:nth-child(2){
+      top: 0;
+      right: 80px;
+      left:initial;
+      animation-delay:0.2s;
+      animation-duration: 3s;
+  }
+
+  &:nth-child(3){
+      top: 80px;
+      right: 0px;
+      left:initial;
+      animation-delay:0.4s ;
+      animation-duration: 2s;
+  }
+
+  &:nth-child(4){
+      top: 0;
+      right: 180px;
+      left:initial;
+      animation-delay:0.6s;
+      animation-duration: 1.5s;
+  }
+
+  &:nth-child(5){
+      top: 0;
+      right: 400px;
+      left:initial;
+      animation-delay:0.8s;
+      animation-duration: 2.5s;
+  }
+
+  &:nth-child(6){
+      top: 0;
+      right: 600px;
+      left:initial;
+      animation-delay:1s ;
+      animation-duration: 3s;
+  }
+  &:nth-child(7 ){
+      top: 300px;
+      right: 0px;
+      left:initial;
+      animation-delay:1s ;
+      animation-duration: 1.75s;
+  }
 `;
