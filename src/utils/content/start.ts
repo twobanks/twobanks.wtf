38675238dc8b3ba725da72ec6acc75e73c960917
twobanks <twobanks@me.com>
2025-12-12 11:38:41 -0300

    fix: menu links

--- src/utils/content/start.ts
+++ src/utils/content/start.ts
@@ -15,18 +15,18 @@ export const menuLinks = [
     name: 'trampos',
     link: '/trampos'
   },
-  {
+  /* {
     name: 'fotos',
     link: '/fotos'
-  },
+  }, */
   {
     name: 'leituras',
     link: '/leituras'
   },
-  {
+  /* {
     name: 'viagens',
     link: '/viagens'
-  },
+  }, */
   {
     name: 'games',
     link: '/games'
